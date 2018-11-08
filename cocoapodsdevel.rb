b15e9a18e799defe424a6e50c8a9ba8285a479fb
Aurora Wright <AuroraWright@users.noreply.github.com>
2018-11-08 15:42:21 +0100

    Add cocoapods devel

---
+++ cocoapodsdevel.rb
@@ -0,0 +1,21 @@
+class Cocoapods < Formula
+  desc "Dependency manager for Cocoa projects"
+  homepage "https://cocoapods.org/"
+  url "https://github.com/CocoaPods/CocoaPods/archive/1.6.0.beta.2.tar.gz"
+  sha256 "0a5e4e1d0fcbed052ba5c4ad0c7510861550012d91134757d5ab33c0316abe3e"
+
+  depends_on "ruby" if MacOS.version <= :sierra
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "cocoapods.gemspec"
+    system "gem", "install", "cocoapods-#{version}.gem"
+    # Other executables don't work currently.
+    bin.install libexec/"bin/pod", libexec/"bin/xcodeproj"
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
+  end
+
+  test do
+    system "#{bin}/pod", "list"
+  end
+end
